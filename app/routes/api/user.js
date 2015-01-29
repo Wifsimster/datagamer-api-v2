d6ce4ba4c383a3c75931ab4d6442ea011d266725
Wifsimster <wifsimster@gmail.com>
2015-01-29 15:55:07 +0100

    Update user route with code and comment.

--- app/routes/api/user.js
+++ app/routes/api/user.js
@@ -25,7 +25,7 @@ module.exports = function () {
             user.name = req.body.name;  // set the users name (comes from the request)
             user.email = req.body.email;
 
-            // save the user and check for errors
+            // Save the user and check for errors
             user.save(function (err) {
                 if (err)
                     res.send(err.message);
@@ -33,25 +33,90 @@ module.exports = function () {
             });
         })
 
-        // Get all the users (accessed at GET http://localhost:8080/api/users)
+        // -----------------------------------------------------------------------------------
+        // --                                       GET                                     --
+        // -----------------------------------------------------------------------------------
+
+        // Description : Get all the users, come with pagination params
+        // URL : http://localhost:8080/api/users/?skip=:skip&limit=:limit
+        // URL params :
+        //          - skip
+        //          - limit
         .get('/users', function (req, res) {
-            User.find(function (err, users) {
+
+            var skip = req.param('skip');
+            var limit = req.param('limit');
+
+            console.log("-- Searching users with skip '" + skip + "' and limit '" + limit + "'...");
+
+            User.find()
+                .skip(skip)
+                .limit(limit)
+                .exec(function (err, users) {
+                    if (err)
+                        res.send(CODE.SERVER_ERROR);
+
+                    var count = users.length;
+                    console.log('-- ' + count + ' user(s) founded !');
+
+                    // Build the response
+                    CODE.SUCCESS.count = count;
+                    CODE.SUCCESS.skip = skip;
+                    CODE.SUCCESS.limit = limit;
+                    CODE.SUCCESS.users = users;
+
+                    res.json(CODE.SUCCESS);
+                });
+        })
+
+        // Description : Get users by a name
+        // URL : http://localhost:8080/api/users/by/name/:name
+        .get('/users/by/name/:user_name', function (req, res) {
+            User.find({name: req.params.user_name}, function (err, users) {
                 if (err)
-                    res.send(err);
-                res.json(users);
+                    res.send(CODE.SERVER_ERROR);
+
+                User.count({name: req.params.user_name}, function (err, count) {
+                    if (err)
+                        res.send(CODE.SERVER_ERROR);
+
+                    console.log(count + ' user(s) founded !');
+
+                    // Build the response
+                    CODE.SUCCESS.count = count;
+                    CODE.SUCCESS.users = users;
+
+                    res.json(CODE.SUCCESS);
+                });
             });
         })
 
-        // Get the user with that id (accessed at GET http://localhost:8080/api/users/:user_id)
-        .get('/users/:user_id', function (req, res) {
+        // Description : Get a user by an id
+        // URL : http://localhost:8080/api/users/by/:id
+        .get('/users/by/id/:user_id', function (req, res) {
             User.findById(req.params.user_id, function (err, user) {
                 if (err)
-                    res.send(err);
-                res.json(user);
+                    res.send(CODE.SERVER_ERROR);
+
+                console.log('Searching for user id ' + req.params.user_id + ' : ' + user.name);
+
+                // Build the response
+                CODE.SUCCESS.user = user;
+
+                res.json(CODE.SUCCESS);
             });
         })
 
-        // Update the user with this id (accessed at PUT http://localhost:8080/api/users/:user_id)
+        // -----------------------------------------------------------------------------------
+        // --                                       PUT                                     --
+        // -----------------------------------------------------------------------------------
+
+        // Description : Update a user with an id
+        // URL : http://localhost:8080/api/users/:user_id
+        // Param :
+        //          - id
+        // Form params :
+        //          - Game Object
         .put('/users/:user_id', function (req, res) {
             // Use our user model to find the user we want
             User.findById(req.params.user_id, function (err, user) {
@@ -64,20 +129,32 @@ module.exports = function () {
                 // Save the user
                 user.save(function (err) {
                     if (err)
-                        res.send(err);
-                    res.json({message: 'User updated!'});
+                        res.send(CODE.SERVER_ERROR);
+
+                    // Build the response
+                    CODE.SUCCESS_PUT.user = user;
+
+                    res.json(CODE.SUCCESS);
                 });
             });
         })
 
-        // Delete the user with this id (accessed at DELETE http://localhost:8080/api/users/:user_id)
+        // -----------------------------------------------------------------------------------
+        // --                                     DELETE                                    --
+        // -----------------------------------------------------------------------------------
+
+        // Description : Delete a user with an id
+        // URL : http://localhost:8080/api/users/:user_id
+        // Param :
+        //          - id
         .delete('/users/:user_id', function (req, res) {
             User.remove({
                 _id: req.params.user_id
             }, function (err, user) {
                 if (err)
-                    res.send(err);
-                res.json({message: 'Successfully deleted'});
+                    res.send(CODE.SERVER_ERROR);
+
+                res.json(CODE.SUCCESS_DELETE);
             });
         });
 
